7b2c3259d7f6179602b97a5646cf01e2f8ac0890
9ziggy9 <davidarogers@protonmail.com>
2024-07-25 18:40:43 -0700

    very simple string view implementation

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 #include <stdbool.h>
 #include <sys/mman.h>
 #include <assert.h>
+#include <string.h>
 
 #define CLOG_PAGE_VIRTUALLY  false
 #define CLOG_PAGE_PHYSICALLY true
 
-typedef struct ClogArena {
+typedef struct {
   size_t size;
   size_t used;
   void *mem_start;
@@ -19,23 +20,38 @@ void *_clog_arena_alloc(ClogArena *, size_t);
 void  _clog_arena_reset(ClogArena *);
 void  _clog_arena_free(ClogArena *);
 
-// 10 kilobytes by default
-#define __CLOG_ARENA_BYTES__ (1024 * 10)
 ClogArena *__CLOG_ARENA__ = NULL;
 
-#define CLOG_INIT()                                                 \
-  do {                                                              \
-    __CLOG_ARENA__ =                                                \
-      _clog_arena_init(__CLOG_ARENA_BYTES__, CLOG_PAGE_PHYSICALLY); \
+#define __CLOG_DEFAULT_ARENA_BYTES__ (1024 * 10) /* 10 kB default */
+
+#define CLOG_INIT()                                                         \
+  do {                                                                      \
+    __CLOG_ARENA__ =                                                        \
+      _clog_arena_init(__CLOG_DEFAULT_ARENA_BYTES__, CLOG_PAGE_PHYSICALLY); \
   } while(0);
 
-const char *CLOG_REGISTER_TYPE_FMT() {
-  return "hello number %d\n";
+#define CLOG_ALLOC(SZ) _clog_arena_alloc(__CLOG_ARENA__, SZ)
+#define CLOG_RESET()   _clog_arena_reset(__CLOG_ARENA__)
+#define CLOG_CLOSE()   _clog_arena_free(__CLOG_ARENA__)
+
+typedef struct { const char *str; size_t len; } ClogString;
+
+ClogString clog_string_make(const char *str) {
+  size_t len = strlen(str);
+  char *buffer = CLOG_ALLOC(len + 1);
+  if (buffer == NULL) assert(0 && "implement panic <-- clog_string_make");
+  memcpy(buffer, str, len + 1);
+  return (ClogString){ buffer, len };
+}
+
+void clog_string_print(ClogString clog_string) {
+  fwrite(clog_string.str, sizeof(char), clog_string.len, stdout);
 }
 
 int main(void) {
   CLOG_INIT();
-  printf(CLOG_REGISTER_TYPE_FMT(), 10);
+  ClogString str = clog_string_make("Hello, world!\n");
+  clog_string_print(str);
   return EXIT_SUCCESS;
 }
 
@@ -44,7 +60,7 @@ ClogArena *_clog_arena_init(size_t bytes, bool page_now) {
   ClogArena *arena = mmap(NULL, sizeof(ClogArena),
                             PROT_READ | PROT_WRITE,
                             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  if (arena == MAP_FAILED) assert(0 && "implement panic");
+  if (arena == MAP_FAILED || arena == NULL) assert(0 && "implement panic");
   arena->size = bytes;
   arena->used = 0;
   arena->mem_start = mmap(NULL, bytes,
@@ -59,4 +75,40 @@ ClogArena *_clog_arena_init(size_t bytes, bool page_now) {
   }
   return arena;
 #undef PAGE_SIZE
+}
+
+void *_clog_arena_alloc(ClogArena *arena, size_t size) {
+  // Round up the allocation size to the nearest multiple of 8 for alignment.
+  size = (size + 7) & ~(size_t)7;
+
+  // Check if there's enough space in the arena otherwise return NULL pointer.
+  if (arena->used + size > arena->size) return NULL;
+
+  // Allocate memory from the arena
+  void *ptr = arena->mem_offset;
+  arena->mem_offset = (char *)arena->mem_offset + size;
+  arena->used += size;
+
+  // Return the allocated memory
+  return ptr;
+}
+
+void _clog_arena_reset(ClogArena *arena) {
+  if (arena != NULL) {
+    arena->used = 0;
+    arena->mem_offset = arena->mem_start;
+  }
+}
+
+void _clog_arena_free(ClogArena *arena) {
+  if (arena != NULL) {
+    // Free the memory allocated for the arena metadata
+    if (munmap(arena, sizeof(ClogArena)) == -1) {
+      assert(0 && "Failed to munmap arena metadata");
+    }
+    // Free the memory allocated for the arena memory block
+    if (munmap(arena->mem_start, arena->size) == -1) {
+      assert(0 && "Failed to munmap arena memory block");
+    }
+  }
 }
